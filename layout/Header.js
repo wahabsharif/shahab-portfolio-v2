86e7101f75bc85956fea813301f4e28342f58a3a
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-05-08 16:25:06 +0500

    Header

--- layout/Header.js
+++ layout/Header.js
@@ -40,7 +40,7 @@ const Header1 = ({ onePageMenu }) => {
                 <Link legacyBehavior href="/">
                   <a>
                     <img
-                      src="assets/images/logos/logo.png"
+                      src="assets/images/logos/shahab-munir-landscape.png"
                       alt="Logo"
                       title="Logo"
                     />
@@ -56,7 +56,7 @@ const Header1 = ({ onePageMenu }) => {
                     <Link legacyBehavior href="/">
                       <a>
                         <img
-                          src="assets/images/logos/logo.png"
+                          src="assets/images/logos/shahab-munir-landscape.png"
                           alt="Logo"
                           title="Logo"
                         />
@@ -106,122 +106,122 @@ const Header1 = ({ onePageMenu }) => {
   );
 };
 
-const Header2 = ({ onePageMenu }) => {
-  useEffect(() => {
-    noxfolioUtilits.stickyNav();
-  }, []);
+// const Header2 = ({ onePageMenu }) => {
+//   useEffect(() => {
+//     noxfolioUtilits.stickyNav();
+//   }, []);
 
-  const toggleSidebar = () => {
-    document.querySelector("body").classList.add("side-content-visible");
-  };
+//   const toggleSidebar = () => {
+//     document.querySelector("body").classList.add("side-content-visible");
+//   };
 
-  const [toggle, setToggle] = useState(false);
+//   const [toggle, setToggle] = useState(false);
 
-  return (
-    <header className="main-header header-two menu-absolute">
-      {/*Header-Upper*/}
-      <div className="header-upper">
-        <div className="container container-1620 clearfix">
-          <div className="header-inner rel d-flex align-items-center">
-            <div className="logo-outer">
-              <div className="logo">
-                <Link legacyBehavior href="/">
-                  <a>
-                    <img
-                      src="assets/images/logos/logo.png"
-                      alt="Logo"
-                      title="Logo"
-                    />
-                  </a>
-                </Link>
-              </div>
-            </div>
-            <div className="nav-outer clearfix mx-auto">
-              {/* Main Menu */}
-              <nav className="main-menu navbar-expand-lg">
-                <div className="navbar-header">
-                  <div className="mobile-logo my-15">
-                    <Link legacyBehavior href="/">
-                      <a>
-                        <img
-                          src="assets/images/logos/logo.png"
-                          alt="Logo"
-                          title="Logo"
-                        />
-                      </a>
-                    </Link>
-                  </div>
-                  {/* Toggle Button */}
-                  <button
-                    type="button"
-                    className="navbar-toggle me-4"
-                    data-bs-toggle="collapse"
-                    data-bs-target=".navbar-collapse"
-                    onClick={() => setToggle(!toggle)}
-                  >
-                    <span className="icon-bar" />
-                    <span className="icon-bar" />
-                    <span className="icon-bar" />
-                  </button>
-                </div>
-                <div
-                  className={`navbar-collapse collapse clearfix ${
-                    toggle ? "show" : ""
-                  }`}
-                >
-                  {onePageMenu ? (
-                    <ul className="navigation onepage clearfix">
-                      <li>
-                        <a href="#home">Home</a>
-                      </li>
-                      <li>
-                        <a href="#about">about</a>
-                      </li>
-                      <li>
-                        <a href="#resume">Resume</a>
-                      </li>
-                      <li>
-                        <a href="#services">services</a>
-                      </li>
-                      <li>
-                        <a href="#skills">skills</a>
-                      </li>
-                      <li>
-                        <a href="#projects">projects</a>
-                      </li>
-                      <li>
-                        <a href="#blog">blog</a>
-                      </li>
-                      <li>
-                        <a href="#contact">Contact</a>
-                      </li>
-                    </ul>
-                  ) : (
-                    <MultiMenu />
-                  )}
-                </div>
-              </nav>
-              {/* Main Menu End*/}
-            </div>
-            {/* Menu Button */}
-            <div className="menu-btns">
-              {/* menu sidbar */}
-              <div className="menu-sidebar d-none d-lg-block">
-                <button onClick={() => toggleSidebar()}>
-                  <img
-                    src="assets/images/shape/sidebar-tottler-white.svg"
-                    alt="Toggler"
-                  />
-                </button>
-              </div>
-              <Link legacyBehavior href="contact">
-                <a className="theme-btn"> let,s talk</a>
-              </Link>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/*End Header Upper*/}
-    </header>
-  );
-};
+//   return (
+//     <header className="main-header header-two menu-absolute">
+//       {/*Header-Upper*/}
+//       <div className="header-upper">
+//         <div className="container container-1620 clearfix">
+//           <div className="header-inner rel d-flex align-items-center">
+//             <div className="logo-outer">
+//               <div className="logo">
+//                 <Link legacyBehavior href="/">
+//                   <a>
+//                     <img
+//                       src="assets/images/logos/logo.png"
+//                       alt="Logo"
+//                       title="Logo"
+//                     />
+//                   </a>
+//                 </Link>
+//               </div>
+//             </div>
+//             <div className="nav-outer clearfix mx-auto">
+//               {/* Main Menu */}
+//               <nav className="main-menu navbar-expand-lg">
+//                 <div className="navbar-header">
+//                   <div className="mobile-logo my-15">
+//                     <Link legacyBehavior href="/">
+//                       <a>
+//                         <img
+//                           src="assets/images/logos/logo.png"
+//                           alt="Logo"
+//                           title="Logo"
+//                         />
+//                       </a>
+//                     </Link>
+//                   </div>
+//                   {/* Toggle Button */}
+//                   <button
+//                     type="button"
+//                     className="navbar-toggle me-4"
+//                     data-bs-toggle="collapse"
+//                     data-bs-target=".navbar-collapse"
+//                     onClick={() => setToggle(!toggle)}
+//                   >
+//                     <span className="icon-bar" />
+//                     <span className="icon-bar" />
+//                     <span className="icon-bar" />
+//                   </button>
+//                 </div>
+//                 <div
+//                   className={`navbar-collapse collapse clearfix ${
+//                     toggle ? "show" : ""
+//                   }`}
+//                 >
+//                   {onePageMenu ? (
+//                     <ul className="navigation onepage clearfix">
+//                       <li>
+//                         <a href="#home">Home</a>
+//                       </li>
+//                       <li>
+//                         <a href="#about">about</a>
+//                       </li>
+//                       <li>
+//                         <a href="#resume">Resume</a>
+//                       </li>
+//                       <li>
+//                         <a href="#services">services</a>
+//                       </li>
+//                       <li>
+//                         <a href="#skills">skills</a>
+//                       </li>
+//                       <li>
+//                         <a href="#projects">projects</a>
+//                       </li>
+//                       <li>
+//                         <a href="#blog">blog</a>
+//                       </li>
+//                       <li>
+//                         <a href="#contact">Contact</a>
+//                       </li>
+//                     </ul>
+//                   ) : (
+//                     <MultiMenu />
+//                   )}
+//                 </div>
+//               </nav>
+//               {/* Main Menu End*/}
+//             </div>
+//             {/* Menu Button */}
+//             <div className="menu-btns">
+//               {/* menu sidbar */}
+//               <div className="menu-sidebar d-none d-lg-block">
+//                 <button onClick={() => toggleSidebar()}>
+//                   <img
+//                     src="assets/images/shape/sidebar-tottler-white.svg"
+//                     alt="Toggler"
+//                   />
+//                 </button>
+//               </div>
+//               <Link legacyBehavior href="contact">
+//                 <a className="theme-btn"> let,s talk</a>
+//               </Link>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//       {/*End Header Upper*/}
+//     </header>
+//   );
+// };
--- layout/Menu.js
+++ layout/Menu.js
@@ -1,42 +1,10 @@
 import Link from "next/link";
 
-const DropdownBtn = () => (
-  <div className="dropdown-btn">
-    <span className="fas fa-chevron-down" />
-  </div>
-);
 const MultiMenu = () => {
   return (
-    <ul className="navigation clearfix">
-      <li className="dropdown">
-        <a href="#">Home</a>
-        <ul>
-          <li className="dropdown">
-            <a href="#">MultiPage</a>
-            <ul>
-              <li>
-                <Link href="/">Home One</Link>
-              </li>
-              <li>
-                <Link href="/index2">Home Two</Link>
-              </li>
-            </ul>
-            <DropdownBtn />
-          </li>
-          <li className="dropdown">
-            <a href="#">OnePage</a>
-            <ul>
-              <li>
-                <Link href="/indexonepage">Home One</Link>
-              </li>
-              <li>
-                <Link href="/index2onepage">Home Two</Link>
-              </li>
-            </ul>
-            <DropdownBtn />
-          </li>
-        </ul>
-        <DropdownBtn />
+    <ul className="navigation text-center clearfix">
+      <li>
+        <Link href="/">Home</Link>
       </li>
       <li>
         <Link href="/about">about</Link>
@@ -44,41 +12,8 @@ const MultiMenu = () => {
       <li>
         <Link href="/services">services</Link>
       </li>
-      <li className="dropdown">
-        <a href="#">projects</a>
-        <ul>
-          <li>
-            <Link href="/projects">project Grid</Link>
-          </li>
-          <li>
-            <Link href="/projects-masonry">projects masonry</Link>
-          </li>
-          <li>
-            <Link href="/project-details">projects details</Link>
-          </li>
-        </ul>
-        <DropdownBtn />
-      </li>
-      <li className="dropdown">
-        <a href="#">blog</a>
-        <ul>
-          <li>
-            <Link href="/blog">blog standard</Link>
-          </li>
-          <li>
-            <Link href="/blog-details">blog details</Link>
-          </li>
-        </ul>
-        <DropdownBtn />
-      </li>
-      <li className="dropdown">
-        <a href="#">pages</a>
-        <ul>
-          <li>
-            <Link href="/404">Error Page</Link>
-          </li>
-        </ul>
-        <DropdownBtn />
+      <li>
+        <Link href="/projects">Projects</Link>
       </li>
       <li>
         <Link href="contact">Contact</Link>
@@ -88,33 +23,33 @@ const MultiMenu = () => {
 };
 export default MultiMenu;
 
-export const OnePageMenu = () => {
-  return (
-    <ul className="navigation onepage clearfix">
-      <li>
-        <a href="#home">Home</a>
-      </li>
-      <li>
-        <a href="#about">about</a>
-      </li>
-      <li>
-        <a href="#resume">Resume</a>
-      </li>
-      <li>
-        <a href="#services">services</a>
-      </li>
-      <li>
-        <a href="#skills">skills</a>
-      </li>
-      <li>
-        <a href="#portfolio">projects</a>
-      </li>
-      <li>
-        <a href="#blog">blog</a>
-      </li>
-      <li>
-        <a href="#contact">Contact</a>
-      </li>
-    </ul>
-  );
-};
+// export const OnePageMenu = () => {
+//   return (
+//     <ul className="navigation onepage clearfix">
+//       <li>
+//         <a href="#home">Home</a>
+//       </li>
+//       <li>
+//         <a href="#about">about</a>
+//       </li>
+//       <li>
+//         <a href="#resume">Resume</a>
+//       </li>
+//       <li>
+//         <a href="#services">services</a>
+//       </li>
+//       <li>
+//         <a href="#skills">skills</a>
+//       </li>
+//       <li>
+//         <a href="#portfolio">projects</a>
+//       </li>
+//       <li>
+//         <a href="#blog">blog</a>
+//       </li>
+//       <li>
+//         <a href="#contact">Contact</a>
+//       </li>
+//     </ul>
+//   );
+// };
